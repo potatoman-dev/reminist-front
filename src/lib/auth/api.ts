a10ebf158e969578cc7f2aa214dd6e540be01414
potatoman-dev <enakatani122@gmail.com>
2024-11-17 17:53:32 +0900

    Add User related pages and features

---
+++ src/lib/auth/api.ts
@@ -0,0 +1,50 @@
+import Cookies from "js-cookie";
+
+import client from "@/lib/api/client";
+
+import { SignInParams, SignUpParams } from "./types";
+
+// サインアップ（新規アカウント作成）
+export const signUp = (params: SignUpParams) => {
+  return client.post("auth", params);
+};
+
+// サインイン（ログイン）
+export const signIn = (params: SignInParams) => {
+  return client.post("auth/sign_in", params);
+};
+
+// サインアウト（ログアウト）
+export const signOut = () => {
+  return client
+    .delete("auth/sign_out", {
+      headers: {
+        "access-token": Cookies.get("_access_token"),
+        client: Cookies.get("_client"),
+        uid: Cookies.get("_uid"),
+      },
+    })
+    .then((response) => {
+      Cookies.remove("_access_token");
+      Cookies.remove("_client");
+      Cookies.remove("_uid");
+      return response;
+    });
+};
+
+// 認証済みのユーザーを取得
+export const getCurrentUser = () => {
+  if (
+    !Cookies.get("_access_token") ||
+    !Cookies.get("_client") ||
+    !Cookies.get("_uid")
+  )
+    return;
+  return client.get("/auth/sessions", {
+    headers: {
+      "access-token": Cookies.get("_access_token"),
+      client: Cookies.get("_client"),
+      uid: Cookies.get("_uid"),
+    },
+  });
+};
