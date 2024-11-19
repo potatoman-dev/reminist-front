e11293c450b687c652470b48ef16400e3068a2ad
potatoman-dev <enakatani122@gmail.com>
2024-11-19 23:27:12 +0900

    add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['var(--font-noto-sans-jp)'],
+      rounded: ['var(--font-m-plus-rounded-1c)'],
+    },
     colors: {
       primary: "#1475D0",
       "primary-hover": "#2B82D4",
