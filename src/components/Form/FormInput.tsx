aa2e1346fd18b9357192014ad933a7280d09bd81
potatoman-dev <enakatani122@gmail.com>
2024-11-21 20:47:13 +0900

    Update FormInput component to display error messages

--- src/components/Form/FormInput.tsx
+++ src/components/Form/FormInput.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from "react";
+import { FieldError } from "react-hook-form";
 
-export const FormInput = (props: { children: ReactNode; label: string }) => {
+export const FormInput = (props: {
+  children: ReactNode;
+  label: string;
+  errors: FieldError | undefined;
+}) => {
   return (
-    <label className="w-full [&_input]:w-full [&_input]:rounded-md [&_input]:border-[1px] [&_input]:border-border [&_input]:bg-surface [&_input]:p-1.5 [&_input]:text-text [&_input]:placeholder:opacity-50">
-      <span className="inline-block pb-1 text-sm font-medium text-text-secondary">
-        {props.label}
-      </span>
-      <br />
-      {props.children}
-    </label>
+    <div>
+      <label className="w-full [&_input]:w-full [&_input]:rounded-md [&_input]:border-[1px] [&_input]:border-border [&_input]:bg-surface [&_input]:p-1.5 [&_input]:text-text [&_input]:placeholder:opacity-50">
+        <span className="inline-block pb-1 text-sm font-medium text-text-secondary">
+          {props.label}
+        </span>
+        <br />
+        {props.children}
+      </label>
+      {props.errors && (
+        <p className="my-1 text-xs text-text-error opacity-80">
+          {props.errors.message}
+        </p>
+      )}
+    </div>
   );
 };
