7c964077472e9ea1e8b6b1a32755b29c3113759f
potatoman-dev <enakatani122@gmail.com>
2024-12-07 15:52:05 +0900

    Add conversation list to person page

--- src/app/(private)/people/[id]/page.tsx
+++ src/app/(private)/people/[id]/page.tsx
@@ -1,13 +1,40 @@
+import Link from "next/link";
+import { FiChevronLeft } from "react-icons/fi";
+
+import { getConversationsList } from "@/features/conversation/api/getConversationsList";
+import { ConversationsList } from "@/features/conversation/components/ConversationsList";
 import { getPerson } from "@/features/person/api/getPerson";
 import { PersonBasicInformation } from "@/features/person/components/PersonBasicInformation";
+import { getAuthTokensServer } from "@/features/user/api/getAuthTokensServer";
 
 const PersonPage = async ({ params }: { params: { id: string } }) => {
-  const data = await getPerson(params.id, "ja");
+  const { accessToken, clientToken, uid } = getAuthTokensServer();
+
+  const person = await getPerson(params.id, "ja");
+  const conversations = await getConversationsList(
+    accessToken,
+    clientToken,
+    uid,
+    params.id
+  );
 
   return (
     <section>
       <div className="mt-6 max-w-3xl px-6 md:mx-auto md:mt-16 lg:w-3/4">
-        <PersonBasicInformation data={data.person} />
+        <Link
+          className="flex w-fit items-center gap-2 text-sm text-text-gray-dark"
+          href="/people"
+        >
+          <FiChevronLeft />
+          ヒトの一覧
+        </Link>
+        <PersonBasicInformation data={person.person} />
+        <div className="mt-16">
+          <ConversationsList
+            conversations={conversations}
+            personId={params.id}
+          />
+        </div>
       </div>
     </section>
   );
