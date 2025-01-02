1b15d5e73e39a30eabe26ce3d926e89501ec17f8
potatoman-dev <enakatani122@gmail.com>
2025-01-02 21:03:20 +0900

    feat: update 404 page with new design and login link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
+import { PublicHeader } from "@/components/Header/PublicHeader";
+
 const NotFound = () => {
   return (
     <div>
-      <h1>404 Not Found</h1>
+      <PublicHeader />
+      <div className="pt-20">
+        <h1 className="text-center text-2xl font-bold">404 Not Found</h1>
+        <p className="mt-3 text-center font-bold text-gray">
+          ページが見つかりませんでした。
+        </p>
+        <div className="mt-20 flex justify-center">
+          <Link
+            className="rounded-full border border-border-white bg-primary px-10 py-3 text-lg font-medium text-white shadow shadow-shadow"
+            href="/signin"
+          >
+            ログインはこちら
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
